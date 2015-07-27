b6055d9b337513fa60f135fb213820c587c22d7d
Aashai1 <aashai123@gmail.com>
2015-07-27 16:06:20 -0700

    Displaying Images

---
+++ DisplayingImages.m
@@ -0,0 +1,37 @@
+
+function [pic_1, pic_2,pic_3 ] = Disp_image(Window, TextureVector, MorphVector)
+
+for I = 1:147
+    
+    pic_1 = random.sample(1:146, 1);
+    pic_2 = pic_1;
+    pic_3 = random.sample(1:146, 1);
+    
+    if pic_3 == pic_2 || pic_3 == pic_1 
+        
+        pic_3 = random.sample(1:
+    
+    temp(I) = Screen('MakeTexture', window, uint8(picture_read_1));
+    
+    Screen('DrawTexture', window, temp(I), [], ...
+        [x_center - x_image/4 , y_center - y_image/4 , x_center + x_image/4, y_center + y_image/4]);
+    
+    
+    
+    % the morph vector 
+    
+    MorphVector = 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+end
+
+
+end
