d0e5f5e04ee53b54218979467f193892b8c5061e
Andrew Si <andrewsi10@gmail.com>
2015-07-27 14:44:24 -0700

    Added Program Layout - please read it

--- test/perception_test.m
+++ test/perception_test.m
@@ -21,10 +21,27 @@ end
 %}
 
 %{
-%keyboard and mouse IO
+%KEYBOARD AND MOUSE IO
 kbCheck(); returns keys that are pressed
 GetMouse(); returns 0 if button not pressed and 1 if pressed
 GetClicks(); returns # of mouse clicks
 reject key presses that aren't relevant to the experiment
 while ~strcmp(kbCheck(), 0) %strcmp returns 1/true if two strings are identical
+%}
+
+%{
+RANDOMIZING STUFF
+Repmat
+repmat([1 2], 1, 4);
+Shuffle
+Shuffle(name_of_vector);
+for i=1:name_of_vector.length
+if name_of_vector(i)==1
+display image on left
+else
+display image on right
+Lock columns with vertcat, size, randperm
+Vertcat
+Randperm
+matrix(:, randperm(size(matrix,2)));
 %}
