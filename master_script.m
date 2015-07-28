aeec65ed3f923e8174b24efbe18bfbcbbd0dddb5
Andrew Si <andrewsi10@gmail.com>
2015-07-28 11:19:03 -0700

    Master Script comments and improved performance

--- master_script.m
+++ master_script.m
@@ -1,18 +1,23 @@
 clear all
-temp = dummyfunction(5);
-TextureVector = imagereader(1);
-MorphVector = imagereader(2);
-imagerandomizer(TextureVector);
+RandStream.setDefaultStream(RandStream('mt19937ar', 'seed', sum(100*clock)));
+[TextureVector, MorphVector] = imagereader(); %TODO
+imagerandomizer(TextureVector); %TODO
+
+%Set up directory to store experiment results
 rootDir = pwd();
 if ~isdir([rootDir '/results/'])
     resultsDir = mkdir([rootDir '/results/']);
 else
     resultsDir = [rootDir '/results/'];
 end
+
+%Set up Screen and Window
 Screen('Preference','SkipSyncTests',1);
 [Window, rect] = Screen('OpenWindow', 0);
 Screen('BlendFunction', Window, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 HideCursor();
+
+%Run the trial and record responses
 numTrials = 10;
 responses = zeros(2, numTrials);
 for i=1:numTrials
@@ -20,6 +25,8 @@ for i=1:numTrials
     responses(1, numTrials) = userinput;
     responses(2, numTrials) = deltamorph;
 end
+
+%Save the results file and close the Screen
 fullPath = fullfile(resultsDir, 'results');
 save(fullPath, 'responses');
 Screen('CloseAll');
