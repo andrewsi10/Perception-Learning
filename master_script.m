f1798b4d7425c5ca67f7085a55729f0f2792a58a
Andrew Si <andrewsi10@gmail.com>
2015-07-28 10:52:06 -0700

    Master Script updated with results file code

--- master_script.m
+++ master_script.m
@@ -3,17 +3,23 @@ temp = dummyfunction(5);
 TextureVector = imagereader(1);
 MorphVector = imagereader(2);
 imagerandomizer(TextureVector);
-ResultsFile = createresultsfile();
+rootDir = pwd();
+if ~isdir([rootDir '/results/'])
+    resultsDir = mkdir([rootDir '/results/']);
+else
+    resultsDir = [rootDir '/results/'];
+end
 Screen('Preference','SkipSyncTests',1);
 [Window, rect] = Screen('OpenWindow', 0);
 Screen('BlendFunction', Window, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 HideCursor();
 numTrials = 10;
-for i=1:10
+responses = zeros(2, numTrials);
+for i=1:numTrials
     [deltamorph, userinput] = imagedisplay(Window, TextureVector, MorphVector);
-    storeresponse(ResultsFile, deltamorph, userinput);
+    responses(1, numTrials) = userinput;
+    responses(2, numTrials) = deltamorph;
 end
-Screen('DrawText', Window, num2str(temp), 1366/2 - 200, 768/2 - 50);
-Screen('Flip', Window);
-WaitSecs(2);
+fullPath = fullfile(resultsDir, 'results');
+save(fullPath, 'responses');
 Screen('CloseAll');
