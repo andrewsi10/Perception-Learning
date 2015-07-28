38fa9e9f54bd1d10edafba76d61dd01b70ea27b3
lalamanlalaman <ericc3350@gmail.com>
2015-07-28 11:51:47 -0600

    some changes

--- readingrandom.m
+++ readingrandom.m
@@ -1,34 +1,44 @@
-function [A, picture1, picture2, randLorR] = readingrandom();
+%function [A, picture1, picture2, randLorR] = readingrandom();
+clear all;
+
+Screen('Preference', 'SkipSyncTests', 1);
+
+[window,rect] = Screen('OpenWindow', 0);
+
+ Screen('BlendFunction', window, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+ HideCursor();
 
 mask = imread('black_new.png');
 
  mask = 255 - mask(:,:,1);
  A = [];
  B = 1:147;
- numtrials = 30;
+ numtrials = 130;
  picture1 = randsample(B, numtrials);
- inc_amount = 4;
+ inc_amount = 2;
  cnt = 0;
  morph_dist = [];
- for i = 1:inc_amount:145
+ for i = 0:inc_amount:10  %
      cnt = cnt + 1;
      morph_dist(cnt) = i;
  end
- Morph_Vector = [];
+ morph_dist = repmat(morph_dist, 1, 26);
+ morph_dist = Shuffle(morph_dist);
  oneortwo = [1, 2];
  largerorsmaller = [];
  picture2  = [];
  for i = 1:numtrials
- Morph_Vector(i) = randsample(morph_dist, 1);
+
  largerorsmaller(i) = randsample(oneortwo, 1);
  if largerorsmaller(i) == 1
-     picture2(i) = mod(picture1(i) - Morph_Vector(i), 147) + 1;
+     picture2(i) = mod(picture1(i) - morph_dist(i), 147) + 1;
  end 
  if largerorsmaller(i) == 2
-     picture2(i) = mod(picture1(i) + Morph_Vector(i), 147) + 1;
+     picture2(i) = mod(picture1(i) + morph_dist(i), 147) + 1;
  end
  end
-
+ picture3 = picture2-picture1;
  leftorright = repmat([1,2], 1, numtrials/2);
  randLorR = Shuffle(leftorright);
  for i = 1:100
@@ -46,8 +56,6 @@ mask = imread('black_new.png');
      C(:,:,4) = mask;
 
      A(i) = Screen('MakeTexture', window, uint8(C));
-
-
     
 
  end
