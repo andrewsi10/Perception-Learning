93ece1f0b80c09412ba908f440000ca16777e2b2
lalamanlalaman <ericc3350@gmail.com>
2015-07-30 16:02:43 -0600

    master_script making circles

---
+++ Project 3/imagedisplay.m
@@ -0,0 +1,60 @@
+function diff = imagedisplay(window, rect, TextureVector)
+
+
+%variables that can be changed
+x_center = rect(3)/2;
+y_center = rect(4)/2;
+image_width = 275;
+image_height = 275;
+average = randi(147);
+%distance between the centers of the images
+radiusofimages = 300;
+% array of degrees from 0 to 300 incrementing by 60 degs each time.
+degrees = [0:60:300];
+
+%draws the circle in the middle
+
+% getMouse
+%    [x_center - image_width/2,y_center - image_height/2,buttons,focus,valuators,valinfo] = GetMouse([windowPtrOrScreenNumber][, mouseDev])
+%    [clicks,x,y,whichButton] = GetClicks([windowPtrOrScreenNumber][, interclickSecs][, mouseDev])
+
+[x,y,buttons] = GetMouse;
+
+%  while any(buttons) % if already down, wait for release
+%        [x,y,buttons] = GetMouse;
+%  end
+%  while ~any(buttons) % wait for press
+%        [x,y,buttons] = GetMouse;
+%  end
+%  while any(buttons) % wait for release
+%        [x,y,buttons] = GetMouse;
+%  end
+random_starter = randi(150);
+vector = [-9 -6 -3 3 6 9];
+starting_x = x;
+while any(buttons)
+   [x,y,buttons] = GetMouse;
+    WaitSecs(0.1);
+end
+while ~any(buttons)
+    % draws the six circles
+    for i = 1:6
+        Screen('DrawTexture', window, TextureVector(mod(average+vector(i), 150) + 1), [], ...
+            [x_center + radiusofimages * cosd(degrees(i)) - image_width/2, y_center - radiusofimages * sind(degrees(i)) - image_height/2, ...
+            x_center + radiusofimages * cosd(degrees(i)) + image_width/2, y_center - radiusofimages * sind(degrees(i)) + image_height/2]);
+    end
+     
+    [x,y,buttons] = GetMouse;
+    forty = floor(abs(mod(random_starter + x - starting_x, 150)) + 1);
+    Screen('DrawTexture', window, TextureVector(forty), [], ...
+        [ x_center - image_width/2, y_center - image_height/2, x_center + image_width/2, y_center + image_height/2]);
+    Screen('Flip', window);
+end
+diff = average - forty;
+
+
+
+
+
+
+
--- Project 3/makingcircles.m
+++ Project 3/makingcircles.m
@@ -1,30 +1,22 @@
 function TextureVector = makingcircles(window)
  
- mask = imread('oval.png');
- 
- mask = 255 - mask(:,:,1);
  TRIANGLE = imread('circle.png');
  factor_size = 1.03;
  for i = 1:75
 
      
     
-     
-     mask = imresize(mask, [272 272]);
-
-     TRIANGLE(:,:,4) = mask;
+  
 
      TextureVector(i) = Screen('MakeTexture', window, uint8(TRIANGLE));
      TRIANGLE = imresize(TRIANGLE, factor_size); 
     
 
  end
+ factor_size = 0.9708737;
  for j = 76:150
-     mask = imresize(mask, [272 272]);
 
-     TRIANGLE(:,:,4) = mask;
-
-     TextureVector(i) = Screen('MakeTexture', window, uint8(TRIANGLE));
-     TRIANGLE = imresize(TRIANGLE, 1/factor_size); 
+     TextureVector(j) = Screen('MakeTexture', window, uint8(TRIANGLE));
+     TRIANGLE = imresize(TRIANGLE, factor_size); 
  end
 end
