165f6337bcfbf31a9541d7704888e3d9d0f87455
lalamanlalaman <ericc3350@gmail.com>
2015-07-31 15:11:29 -0600

    some chanjes

--- Project 3/makingcircles.m
+++ Project 3/makingcircles.m
@@ -6,20 +6,22 @@ function [TextureVector ImageWidth] = makingcircles(window)
  mask = imresize(mask, [272 272]);
 
  TRIANGLE = imread('circle.png');
+ TRIANGLE = imresize(TRIANGLE, [272 272]);
  TRIANGLE(:, :, 4) = mask;
- factor_size = 0.98;
+
+ factor_size = 3;
  ImageWidth(1) = 272;
  for i = 1:75
      TextureVector(i) = Screen('MakeTexture', window, uint8(TRIANGLE));
-     TRIANGLE = imresize(TRIANGLE, factor_size); 
-     ImageWidth(i + 1) = ImageWidth(i) * factor_size;
+     TRIANGLE = imresize(TRIANGLE, [ImageWidth(i) ImageWidth(i)]); 
+     ImageWidth(i + 1) = ImageWidth(i) - factor_size;
  end
  
  for j = 76:150
 
      TextureVector(j) = Screen('MakeTexture', window, uint8(TRIANGLE));
-     TRIANGLE = imresize(TRIANGLE, 1.0/factor_size); 
-     ImageWidth(j + 1) = ImageWidth(j) / factor_size;
+     TRIANGLE = imresize(TRIANGLE, [ImageWidth(j) ImageWidth(j)]); 
+     ImageWidth(j + 1) = ImageWidth(j) + factor_size;
   
  end
  
