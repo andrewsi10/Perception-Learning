7be51f0641d8efaf5fd4a8484cf6bc0ec6d26e8b
Aashai1 <aashai123@gmail.com>
2015-08-06 10:35:31 -0700

    stuff and stuff

---
+++ Lego Ensemble Coding project/Readingimages.m
@@ -0,0 +1,26 @@
+function TextureVector = readingin(window)
+ 
+ mask = imread('lego brick.png');
+ 
+ mask = 255 - mask(:,:,1);
+ 
+ for i = 1:147
+
+     
+    if i >= 10
+     TRIANGLE = imread([num2str(i) '.png']);
+    end
+    if i < 10
+     TRIANGLE = imread([num2str(0) num2str(i) '.png'] );
+    end
+
+     C = imresize(TRIANGLE, [275 275]);
+     mask = imresize(mask, [275 275]);
+
+     C(:,:,4) = mask;
+
+     TextureVector(i) = Screen('MakeTexture', window, uint8(C));
+    
+
+ end
+end
